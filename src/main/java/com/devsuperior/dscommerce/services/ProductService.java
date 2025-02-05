453967956697c8a4a3b87f1feca01e1c97c74d97
Arthur Oliveira <arthur08oliveira01@gmail.com>
2025-02-05 18:00:53 -0300

    Post - Inserindo novo produto

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -26,4 +26,18 @@ public class ProductService {
         Page<Product> result = repository.findAll(pageable);
         return result.map(ProductDTO::new);
     }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity = repository.save(entity);
+
+        return new ProductDTO(entity);
+    }
 }
