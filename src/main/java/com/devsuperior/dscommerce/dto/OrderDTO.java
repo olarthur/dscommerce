924f7ad4319e8b32056327e1a78cb66e45126649
Arthur Oliveira <arthur08oliveira01@gmail.com>
2025-03-06 15:56:36 -0300

    POST new Order

--- src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscommerce.dto;
 import com.devsuperior.dscommerce.entities.Order;
 import com.devsuperior.dscommerce.entities.OrderItem;
 import com.devsuperior.dscommerce.entities.OrderStatus;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.time.Instant;
 import java.util.ArrayList;
@@ -18,7 +19,11 @@ public class OrderDTO {
 
     private PaymentDTO payment;
 
+    @NotEmpty(message = "Deve conter ao menos um item")
     private List<OrderItemDTO> items = new ArrayList<>();
+
+    public OrderDTO() {
+    }
 
     public OrderDTO(Long id, Instant moment, OrderStatus status, ClientDTO client, PaymentDTO payment) {
         this.id = id;
